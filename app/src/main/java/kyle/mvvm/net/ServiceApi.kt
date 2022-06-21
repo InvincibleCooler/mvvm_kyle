64ceb1f38984218a8198cf92e97221353d7625ba
invin <invin.v@kakaocorp.com>
2022-06-21 17:34:22 +0900

    service api 수정

--- app/src/main/java/kyle/mvvm/net/ServiceApi.kt
+++ app/src/main/java/kyle/mvvm/net/ServiceApi.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Path
 
 
 interface ServiceApi {
-    // eg) https://api.itbook.store/1.0/search/kotlin
-    @GET("search/{query}")
-    fun getSearch(@Path("query") query: String): Call<SearchRes>
+    // eg) https://api.itbook.store/1.0/search/kotlin/1
+    @GET("search/{query}/{page}")
+    suspend fun getSearch(@Path("query") query: String, @Path("page") page: String): SearchRes
 }
